266ffadf8b5781fd421f87bb25bdf41fc388038d
PSNAppz <psnappz@gmail.com>
2016-09-07 21:03:45 +0530

    Auth added
    Added Auth

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -75,15 +75,11 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    May I Help You?
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Documentation</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <a href="#"></a>
                 </div>
             </div>
         </div>
