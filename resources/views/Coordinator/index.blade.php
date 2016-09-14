0981c2afd54d6f3dc23eb010b763694e5fc2d29a
PSNAppz <psnappz@gmail.com>
2016-09-14 21:30:22 +0530

    test

--- resources/views/Coordinator/index.blade.php
+++ resources/views/Coordinator/index.blade.php
@@ -1,5 +1,6 @@
 <html>
 <head>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
 
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -25,7 +26,6 @@
 <!-- DATA TABLES DONOT TOUCH-->
 
         <link href="https://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.css" rel="stylesheet" />
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
         <script src="https://cdn.datatables.net/1.10.7/js/jquery.dataTables.min.js"></script>
         <script src="https://cdn.datatables.net/plug-ins/1.10.7/integration/bootstrap/3/dataTables.bootstrap.js"></script>
 
