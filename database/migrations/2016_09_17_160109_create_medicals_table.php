92b2b812a799f331adc6e42b6a93286b0e893cde
Markandeya <vasalpha@gmail.com>
2016-09-17 22:56:16 +0530

    Medical module half completed

---
+++ database/migrations/2016_09_17_160109_create_medicals_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMedicalsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('medicals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('loc');
+            $table->string('doc');
+            $table->string('contact');
+            $table->string('dis');
+            $table->string('side');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('medicals');
+    }
+}
