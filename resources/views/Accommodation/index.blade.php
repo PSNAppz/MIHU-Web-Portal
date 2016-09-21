d5827e1932199b4a7dd2832ad4b71dff29dbd114
Markandeya <vasalpha@gmail.com>
2016-09-21 06:46:28 +0530

    Accomodation completed

--- resources/views/Accommodation/index.blade.php
+++ resources/views/Accommodation/index.blade.php
@@ -52,6 +52,8 @@
                                 <th>Accommodation At</th>
                                 <th>Near By</th>
                                 <th>Status</th>
+                                <th>Coordinator</th>
+                                <th>Phone</th>
                                 <th>Updated at</th>
                                 @if(!Auth::guest())
                                 <th></th>
@@ -71,6 +73,8 @@
                                                 <span class="label label-success">Available</span>
                                             @endif</th>
 
+                                        <th>{{ $acc->coord}}</th>
+                                        <th>{{ $acc->contact}}</th>
                                         <th>{{ $acc->updated_at}}</th>
                                         @if(!Auth::guest())
                                         <th><a class="btn btn-warning" href="{{ route('accommodation.edit', $acc->id,'/edit') }}" role="button">Update</a></th>
@@ -110,6 +114,8 @@
                                     <th>Accommodation At</th>
                                     <th>Near By</th>
                                     <th>Status</th>
+                                    <th>Coordinator</th>
+                                    <th>Phone</th>
                                     <th>Updated at</th>
                                     @if(!Auth::guest())
                                     <th></th>
@@ -129,6 +135,8 @@
                                                     <span class="label label-success">Available</span>
                                                 @endif</th>
 
+                                            <th>{{ $acc->coord}}</th>
+                                            <th>{{ $acc->contact}}</th>
                                             <th>{{ $acc->updated_at}}</th>
                                             @if(!Auth::guest())
                                             <th><a class="btn btn-warning" href="{{ route('accommodation.edit', $acc->id,'/edit') }}" role="button">Update</a></th>
@@ -169,6 +177,8 @@
                             <th>Accommodation At</th>
                             <th>Near By</th>
                             <th>Status</th>
+                            <th>Coordinator</th>
+                            <th>Phone</th>
                             <th>Updated at</th>
                             @if(!Auth::guest())
                             <th></th>
@@ -188,6 +198,8 @@
                                             <span class="label label-success">Available</span>
                                         @endif</th>
 
+                                    <th>{{ $acc->coord}}</th>
+                                    <th>{{ $acc->contact}}</th>
                                     <th>{{ $acc->updated_at}}</th>
                                     @if(!Auth::guest())
                                     <th><a class="btn btn-warning" href="{{ route('accommodation.edit', $acc->id,'/edit') }}" role="button">Update</a></th>
@@ -214,6 +226,8 @@
                             <th>From</th>
                             <th>Accommodation At</th>
                             <th>Near By</th>
+                            <th>Coordinator</th>
+                            <th>Phone</th>
                             <th>Status</th>
                             <th>Updated at</th>
                             @if(!Auth::guest())
@@ -234,6 +248,8 @@
                                             <span class="label label-success">Available</span>
                                         @endif</th>
 
+                                    <th>{{ $acc->coord}}</th>
+                                    <th>{{ $acc->contact}}</th>
                                     <th>{{ $acc->updated_at}}</th>
                                     @if(!Auth::guest())
                                     <th><a class="btn btn-warning" href="{{ route('accommodation.edit', $acc->id,'/edit') }}" role="button">Update</a></th>
@@ -258,12 +274,64 @@
 
           <div id="vip" class="tab-pane fade">
       <h3>VIP</h3>
-      <p>The below chart shows the no of visits calculated in months of present year.</p>
+      <div class="container">
+          <div class="row">
+              <div class="col-md-10 col-md-offset-1">
+                  @if(!Auth::guest())
+                  <a class="btn btn-success" href="{{ url('/accommodation/create') }}" role="button">Add New Accommodation Details</a>
+              @endif
+                  <hr>
+                  <div class="panel panel-default">
+                      <div class="panel-body">
+                          <div style="overflow-x:auto;">
+                          <table class="table">
+                              <thead>
+                                  <th>From</th>
+                                  <th>Accommodation At</th>
+                                  <th>Near By</th>
+                                  <th>Status</th>
+                                  <th>Coordinator</th>
+                                  <th>Phone</th>
+                                  <th>Updated at</th>
+                                  @if(!Auth::guest())
+                                  <th></th>
+                                  <th></th>
+                              @endif
+                              </thead>
+                              <tbody>
+                                  @foreach($accommodations as $acc)
+                                      <tr>
+                                      @if($acc->gender==4)
+                                          <th>{{ $acc->areaName}}</th>
+                                          <th>{{ $acc->locationofAcc}}</th>
+                                          <th>{{ $acc->nearby}}</th>
+                                          <th>@if ($acc->isFull == 1)
+                                                  <span class="label label-danger">Accommodation Full</span>
+                                              @else
+                                                  <span class="label label-success">Available</span>
+                                              @endif</th>
 
-      <br>
-      <br>
-      <br>
-      <br>
+                                          <th>{{ $acc->coord}}</th>
+                                          <th>{{ $acc->contact}}</th>
+                                          <th>{{ $acc->updated_at}}</th>
+                                          @if(!Auth::guest())
+                                          <th><a class="btn btn-warning" href="{{ route('accommodation.edit', $acc->id,'/edit') }}" role="button">Update</a></th>
+                                          <th>  {{ Form::open(['method' => 'DELETE', 'route' => ['accommodation.destroy', $acc->id]]) }}
+                                          {{ Form::submit('Delete', ['class' => 'btn btn-danger']) }}
+                                          {{ Form::close() }}</th>
+                                          @endif
+                                      @endif
+                                      </tr>
+                                  @endforeach
+                              </tbody>
+                          </table>
+                      </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+      {{$accommodations->links()}}
 
     </div>
 
