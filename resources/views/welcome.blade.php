af3b8c94ec87ca6f584e9a074a0bcbd9dd53b0af
PSNAppz <psnappz@gmail.com>
2016-09-27 13:18:42 +0530

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -184,7 +184,9 @@
 
    <div class = "flex-center position-ref full-height">
         </div>
-
+<marquee style="color: #990000; font-size:22px;">
+       <b> <a href="http://www.amrita.ac.in/live/">Watch Amritavarsham live click here...</a> </b>
+      </marquee>
        <div class="jumbotron" style="margin-bottom: 0px">
          <div class ="button">
            <p><b>Select the department</b></p>
@@ -302,47 +304,6 @@
          </div>
      </div>
  </div>
-
-
- <div id="carousel-example-generic" class="carousel slide" data-ride="carousel">
-  <!-- Indicators -->
-  <ol class="carousel-indicators">
-    <li data-target="#carousel-example-generic" data-slide-to="0" class="active"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-    <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-  </ol>
-
-  <!-- Wrapper for slides -->
-  <div class="carousel-inner" role="listbox">
-    <div class="item active">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-          <center><img src="{{asset('/images/missing1.jpg')}}">
-          <h4><b>Missing since 6pm 25th Sept</b></h4>
-          <h4><p>Name : Sanju Sanal
-              From Amritavidyalayam, Kodungalloor.</p>
-          <p> If found please contact: Anil Ph: 9744443334 , 8606519234 , 9447480134</h4></center>
-      </div>
-    </div>
-    <div class="item">
-      <img src="..." alt="...">
-      <div class="carousel-caption">
-        ...
-      </div>
-    </div>
-    ...
-  </div>
-
-  <!-- Controls -->
-  <a class="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-    <span class="sr-only">Previous</span>
-  </a>
-  <a class="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
   @include('layouts.footer')
     </body>
 </html>
